87d5ce58be78e533278db93113a6c15d6bfdc375
Medenchi <denlifik@gmail.com>
2026-01-17 02:01:35 +0300

    Create DelHomeCommand.java

---
+++ src/main/java/com/plasma/core/commands/DelHomeCommand.java
@@ -0,0 +1,38 @@
+package com.plasma.core.commands;
+
+import com.plasma.core.PlasmaCore;
+import com.plasma.core.utils.MessageUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class DelHomeCommand implements CommandExecutor {
+
+    private final PlasmaCore plugin;
+
+    public DelHomeCommand(PlasmaCore plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("§cТолько для игроков!");
+            return true;
+        }
+
+        if (!plugin.getAuthManager().isLoggedIn(player)) {
+            MessageUtils.send(player, "login-required");
+            return true;
+        }
+
+        if (args.length < 1) {
+            MessageUtils.sendRaw(player, "&cИспользование: /delhome <название>");
+            return true;
+        }
+
+        plugin.getHomesManager().deleteHome(player, args[0]);
+        return true;
+    }
+}
