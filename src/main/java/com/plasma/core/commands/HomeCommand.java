b1abcb69e5c1f87001543bab592106e2d17903d2
Medenchi <denlifik@gmail.com>
2026-01-17 02:00:17 +0300

    Create HomeCommand.java

---
+++ src/main/java/com/plasma/core/commands/HomeCommand.java
@@ -0,0 +1,34 @@
+package com.plasma.core.commands;
+
+import com.plasma.core.PlasmaCore;
+import com.plasma.core.utils.MessageUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HomeCommand implements CommandExecutor {
+
+    private final PlasmaCore plugin;
+
+    public HomeCommand(PlasmaCore plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("§cТолько для игроков!");
+            return true;
+        }
+
+        if (!plugin.getAuthManager().isLoggedIn(player)) {
+            MessageUtils.send(player, "login-required");
+            return true;
+        }
+
+        String homeName = args.length > 0 ? args[0] : "home";
+        plugin.getHomesManager().teleportHome(player, homeName);
+        return true;
+    }
+}
